d156a9b49c9a82fd2df5bfbf482c6e0e754d9868
andy <ztao8607@gmail.com>
2020-07-21 11:42:31 +0800

    添加环境标示

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,5 +1,11 @@
 // export const DOMAIN = 'http://127.0.0.1:8000'
-export const DOMAIN = 'https://ai.devexp.cn/service'
+var _DOMAIN = 'http://127.0.0.1:8000'
+
+if (process.env.NODE_ENV === 'production') {
+    _DOMAIN = 'https://ai.devexp.cn/service'
+}
+
+export const DOMAIN = _DOMAIN
 export const CDNTOKEN = DOMAIN + '/cdn/token'
 export const CDNFILENAME = DOMAIN + '/cdn/upload/'
 export const CDNPREFILENAME = DOMAIN + '/prediction/result'
